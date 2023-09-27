8c9d6856b3c9e4489abc799429daf4b7615d953c
Anthony Lara <Capone0824@gmail.com>
2023-09-27 12:48:47 -0400

    first commit

--- app.js
+++ app.js
@@ -42,4 +42,14 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(x => {
+    console.log(`Connected to Mongo database: "${x.connections[0].name}"`);
+  })
+  .catch(err => {
+    console.log(`An error occurred while connecting to the Database: ${err}`);
+  });
+
+
 module.exports = app;
